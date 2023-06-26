973d66fd6bb0ae3bd91076d40ecbf3667ee6c7d4
ciolfi <ciolfi2@gmail.com>
2023-06-26 16:46:11 -0400

    Redeploy with Google info 3

--- next.config.js
+++ next.config.js
@@ -10,9 +10,7 @@ const nextConfig = {
     'MYSQL_HOST': 'db.unitedwayatlantaefsp.org',
     'MYSQL_DATABASE': 'unitedwayatlantaefsp',
     'MYSQL_USER': 'efsp_mngr',
-    'MYSQL_PASSWORD':'TKL72wvu$',
-    'GOOGLE_CLIENT_ID':'91399484004-6mg53m6mcoul9fgr3i0crimncjnic0g2.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET':'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'
+    'MYSQL_PASSWORD':'TKL72wvu$'
   }
 }
 
