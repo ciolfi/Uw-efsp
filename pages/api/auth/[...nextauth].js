d000a8de4eec241ea100873b520d0dee41427465
ciolfi <ciolfi2@gmail.com>
2023-06-27 15:08:09 -0400

    Add Google stuff

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,12 @@ import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: "844640573338-sapsnsiuocmal1gll5a4ds4sel6rlv4f.apps.googleusercontent.com",
-            clientSecret: "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",
+            // clientId: "844640573338-sapsnsiuocmal1gll5a4ds4sel6rlv4f.apps.googleusercontent.com",
+            // clientSecret: "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",
+            // clientId: "314723659024-34m384tmk9ce66d0qjdvbut4ub6u92e7.apps.googleusercontent.com",
+            // clientSecret: "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",
+            clientId: "828799001592-onjlep0eo6etr2gtv70j501o3ik401jj.apps.googleusercontent.com",
+            clientSecret: "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",
             authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
         })
     ],
